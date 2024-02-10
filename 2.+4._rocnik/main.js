3efd4d8d25fcc7d2186facd11685fcda66daa9c3
Cuno-oss <jakub.cuninka1@gmail.com>
2024-02-10 08:47:41 +0100

    commit

---
+++ 2.+4._rocnik/main.js
@@ -0,0 +1,9 @@
+
+function OpenNav() {
+    var element = document.getElementById("side_nav");
+    element.classList.add("activesidebar");
+ }
+ function CloseNav() {
+  var element = document.getElementById("side_nav");
+  element.classList.remove("activesidebar");
+ }
