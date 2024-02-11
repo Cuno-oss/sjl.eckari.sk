1d0dfe3b692590672b566c1cc5179d40d11d483e
Cuno-oss <jakub.cuninka1@gmail.com>
2024-02-11 12:37:39 +0100

    iba 1 a 3 rocnik

--- main.js
+++
@@ -1,9 +0,0 @@
-
-function OpenNav() {
-    var element = document.getElementById("side_nav");
-    element.classList.add("activesidebar");
- }
- function CloseNav() {
-  var element = document.getElementById("side_nav");
-  element.classList.remove("activesidebar");
- }
